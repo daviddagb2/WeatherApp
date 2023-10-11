41f21ce1512419a19b722ae25681eb384318d5e6
Alexander Miguel Gonzalez <alexmgb3@gmail.com>
2023-10-11 11:31:38 -0600

    Update constants

--- app/src/main/java/com/gonzalez/blanchard/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/gonzalez/blanchard/weatherapp/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.gonzalez.blanchard.weatherapp.utils
 
 object Constants {
-    const val MAPBOX_TOKEN = "sk.eyJ1IjoiZGF2aWRnYjc3NyIsImEiOiJjbG5qM2E0aGMxZjl6Mm5wZXUwdXliZGo0In0.uVRbPj4KDdiWF-XnljF3SQ"
+    const val MAPBOX_TOKEN = "INSERT_TOKEN_HERE"
 }
